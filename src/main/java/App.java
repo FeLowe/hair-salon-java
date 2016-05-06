cafc3824007d1a26beb42db5d6a53df99fd0389d
Fernanda Lowe <fe_lowe@hotmail.com>
2016-05-06 14:19:34 -0700

    fail to show stylist page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,6 +20,20 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("stylits", Stylist.all());
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
+      post("/stylists", (request, response) -> {
+           HashMap<String, Object> model = new HashMap<String, Object>();
+           String stylistName = request.queryParams("stylist-name");
+           Stylist newStylist = new Stylist(stylistName);
+           newStylist.save();
+           response.redirect("/");
+           return null;
+         });
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -23,11 +23,32 @@ public class AppTest extends FluentTest {
   public static ServerRule server = new ServerRule();
 
   @Test
-    public void rootTest() {
-      goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("Lowe's Hair Salon");
-      assertThat(pageSource()).contains("Hairstylist");
-      assertThat(pageSource()).contains("add hairstylist");
-    }
+   public void rootTest() {
+     goTo("http://localhost:4567/");
+     assertThat(pageSource()).contains("Lowe's Hair Salon");
+     assertThat(pageSource()).contains("hairstylist");
+     assertThat(pageSource()).contains("client");
+   }
+    //
+    //  @Test
+    //    public void StylistIsDisplayedTest() {
+    //   Stylist newStylist = new Stylist ("Sabrina");
+    //   newStylist.save();
+    //   String stylistPath = String.format("http://localhost:4567/stylists/%d", newStylist.getId());
+    //   goTo(stylistPath);
+    //   assertThat(pageSource()).contains("Sabrina");
+    // }
+  @Test
+  public void StylistShowPage() {
+    Stylist newStylist = new Stylist("Sabrina");
+    newStylist.save();
+    Client newClient = new Client("Lataevia", newClient.getId());
+    newClient.save();
+    String stylistPath = String.format("http://localhost:4567/stylists/%d", newStylist.getId());
+    goTo(stylistPath);
+    click("a", withText("Lataevia"));
+    assertThat(pageSource()).contains("Lataevia");
+    assertThat(pageSource()).contains("Go back");
+  }
 
 }
